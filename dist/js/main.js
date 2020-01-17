af26c68104ebb08fb367990350db0920ff026d40
Serj1c <sergej.a.isaev@gmail.com>
2020-01-17 22:56:15 +0300

    menu button and some js

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set menu state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set menu state
+        showMenu = false;
+    }
+}
+
